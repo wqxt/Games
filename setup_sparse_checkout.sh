fd27f9babd22f02632aa6f073b04058fe9f37675
wqxt <vladimirdeveloperwork@gmail.com>
2024-10-22 19:36:35 +0300

    Setuped sparse-checkout file.

--- setup_sparse_checkout.sh
+++ setup_sparse_checkout.sh
@@ -6,33 +6,30 @@ if [ ! -d ".git" ]; then
     exit 1
 fi
 
-# Сброс настроек sparse-checkout для главного репозитория
-echo "Сброс настроек sparse-checkout для главного репозитория..."
-git sparse-checkout disable  # Отключение sparse-checkout
-git checkout .                # Восстановление всех файлов
-
 # Инициализация сабмодулей
 echo "Инициализация сабмодулей..."
 git submodule update --init --recursive
 
-# Настройка sparse-checkout для сабмодуля IdleRpg
-echo "Инициализируй sparse-checkout для сабмодуля IdleRpg..."
+# Настройка sparse-checkout для сабмодуля IdleRpg и переключение на ветку main
+echo "Инициализация sparse-checkout и переключение на ветку main для сабмодуля IdleRpg..."
 cd Assets/ExternalGames/IdleRpg || exit
+git checkout main || git checkout -b main  # Переключение на ветку main, если она существует, или создание
+git pull origin main  # Подтянуть изменения из ветки main
 git sparse-checkout init --cone                      
 git sparse-checkout set Assets/_IdleRpgGame                            
-cd -                                                
+cd -
 
-# Обновление сабмодуля IdleRpg
-git submodule update --init --recursive              
-
-# Настройка sparse-checkout для сабмодуля Platformer
-echo "Инициализируй sparse-checkout для сабмодуля Platformer..."
+# Настройка sparse-checkout для сабмодуля Platformer и переключение на ветку main
+echo "Инициализация sparse-checkout и переключение на ветку main для сабмодуля Platformer..."
 cd Assets/ExternalGames/Platformer || exit
+git checkout main || git checkout -b main  # Переключение на ветку main, если она существует, или создание
+git pull origin main  # Подтянуть изменения из ветки main
 git sparse-checkout init --cone                     
 git sparse-checkout set Assets/_Platformer 
-cd -                                                 
+cd -
 
-# Обновление сабмодуля Platformer
-git submodule update --init --recursive               
+# Обновление сабмодулей
+echo "Обновление сабмодулей..."
+git submodule update --remote --recursive       
 
-echo "Сабмодули успешно настроены с sparse-checkout!"
+echo "Сабмодули успешно настроены!"
