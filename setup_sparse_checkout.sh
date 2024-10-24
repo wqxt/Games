ba2270b2435bcb3bd1da24d9e860add235a040c7
wqxt <vladimirdeveloperwork@gmail.com>
2024-10-24 11:52:54 +0300

    Setuped sparse file.Default branch is developsubmodule.

--- setup_sparse_checkout.sh
+++ setup_sparse_checkout.sh
@@ -6,32 +6,43 @@ if [ ! -d ".git" ]; then
     exit 1
 fi
 
-# Инициализация сабмодулей
 echo "Инициализация сабмодулей..."
 git submodule update --init --recursive
 
-read -p "Введите имя ветки для сабмодуля IdleRpg (по умолчанию 'main'): " idle_branch
-idle_branch=${idle_branch:-main}  
+
+
+read -p "Введите имя ветки для сабмодуля IdleRpg (по умолчанию 'developsubmodule'): " idle_branch
+idle_branch=${idle_branch:-developsubmodule}  
+
+echo "Обновление ветки для сабмодуля IdleRpg в .gitmodules..."
+git config -f .gitmodules submodule.Assets/ExternalGames/IdleRpg.branch "$idle_branch"
 
 echo "Инициализация sparse-checkout и переключение на ветку '$idle_branch' для сабмодуля IdleRpg..."
 cd Assets/ExternalGames/IdleRpg || exit
-git checkout "$idle_branch" || git checkout -b "$idle_branch"  
-git pull origin "$idle_branch"  
-git sparse-checkout init --cone                      
-git sparse-checkout set Assets/_IdleRpgGame                            
+git checkout "$idle_branch" || git checkout -b "$idle_branch"
+git pull origin "$idle_branch"
+git sparse-checkout init --cone
+git sparse-checkout set Assets/_IdleRpgGame
 cd -
 
-read -p "Введите имя ветки для сабмодуля Platformer (по умолчанию 'main'): " platformer_branch
-platformer_branch=${platformer_branch:-main}  
+
+
+read -p "Введите имя ветки для сабмодуля Platformer (по умолчанию 'developsubmodule'): " platformer_branch
+platformer_branch=${platformer_branch:-developsubmodule}  
+
+echo "Обновление ветки для сабмодуля Platformer в .gitmodules..."
+git config -f .gitmodules submodule.Assets/ExternalGames/Platformer.branch "$platformer_branch"
 
 echo "Инициализация sparse-checkout и переключение на ветку '$platformer_branch' для сабмодуля Platformer..."
 cd Assets/ExternalGames/Platformer || exit
-git checkout "$platformer_branch" || git checkout -b "$platformer_branch" 
-git pull origin "$platformer_branch"  
-git sparse-checkout init --cone                     
-git sparse-checkout set Assets/_Platformer 
+git checkout "$platformer_branch" || git checkout -b "$platformer_branch"
+git pull origin "$platformer_branch"
+git sparse-checkout init --cone
+git sparse-checkout set Assets/_Platformer
 cd -
 
+
+
 echo "Обновление сабмодулей..."
-git submodule update --remote --recursive       
+git submodule update --remote --recursive
 echo "Сабмодули успешно настроены!"
